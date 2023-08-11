10bff77eef530019161c57e92660adb0dd702615
anupBH <anupbhaumik44@gmail.com>
2023-08-11 22:38:20 +0530

    bug

--- bingo-ui/src/bingo/context-api/bCountProvider.js
+++ bingo-ui/src/bingo/context-api/bCountProvider.js
@@ -2,12 +2,11 @@ import React, {createContext,useContext,useState} from "react";
 import { useNavigate } from "react-router";
 const BingoCounterContext = createContext();
 export function BingoCounterProvider({children}){
-    const nevigate=useNavigate();
+    
     const [bingoCount,setBingoCount]= useState(0);
     const incBingoCount=(count)=>{
         if((count+bingoCount)>=5)
         {
-            nevigate('/')
             setBingoCount(5);
         }
             else{
