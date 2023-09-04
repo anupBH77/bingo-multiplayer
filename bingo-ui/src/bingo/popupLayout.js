64a5115fca440cf09a2bfc0914b060977692bc9f
anupBH <anupbhaumik44@gmail.com>
2023-09-04 14:26:42 +0530

    added custom room feature

---
+++ bingo-ui/src/bingo/popupLayout.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PopupLayout = (props) => {
+  return (
+    <div
+      class="fixed inset-0 flex items-center justify-center z-50 bg-gray-600 bg-opacity-30  "
+      id="popup"
+    >
+      <div class=" flex justify-center items-center  p-2  bg-white  w-[18rem] h-[10rem] text-center shadow-2xl shadow-gray-500">
+        {props.children}
+        
+      </div>
+    </div>
+  );
+};
+
+export default PopupLayout;
---
+++ server/ioListen.js
@@ -0,0 +1,4 @@
+function ioListen(io) {
+    
+    
+}
